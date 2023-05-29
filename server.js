8777f6172a9853ca3a70615c65540117766b547e
Marco Obinu <marco.obinu@omegamadlab.com>
2023-05-29 19:09:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 80
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
+  response.end('Info: ' + process.env + '\n')
 })
 
 server.listen(port)
